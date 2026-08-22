9e04641caa8ea550fbdf77e586823396dc3c2aa1
Jack1Constantine@outlook.com <jack1constantine@outlook.com>
2026-08-22 22:42:18 +0800

    fix

---
+++ send_feishu.py
@@ -0,0 +1,30 @@
+import os
+import requests
+
+
+FEISHU_WEBHOOK = os.environ["FEISHU_WEBHOOK"]
+
+
+def send_message(text):
+
+    payload = {
+        "msg_type": "text",
+        "content": {
+            "text": text
+        }
+    }
+
+
+    r = requests.post(
+        FEISHU_WEBHOOK,
+        json=payload,
+        timeout=10
+    )
+
+    r.raise_for_status()
+
+
+if __name__ == "__main__":
+    send_message(
+        "每天12点自动提醒"
+    )
